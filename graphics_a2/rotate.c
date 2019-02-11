938f215ed49308de70634d2274a36cd8fb22ca9d
gideon <gideonrich@hotmail.ca>
2019-02-10 22:37:57 -0700

    Change some variables and add some comments

--- graphics_a2/rotate.c
+++ graphics_a2/rotate.c
@@ -1,5 +1,5 @@
-#define PROGRAM_TITLE "Assignment 2 Graphics 3710"
-#define DISPLAY_INFO "Assignment 2 Graphics 3710"
+#define PROGRAM_TITLE "Assignment 2 Gideon Richter Graphics 3710"
+#define DISPLAY_INFO "Assignment 2 Gideon Richter Graphics 3710"
 
 #include <stdlib.h>  // Useful for the following includes.
 #include <stdio.h>    
@@ -9,31 +9,23 @@
 #include <GL/glu.h>  // GLU support library.
 #include <GL/glut.h> // GLUT support library.
 
-// Some global variables.
-// Window IDs, window width and height.
-int Window_ID;
-int Window_Width = 600;
-int Window_Height = 400;
 
-// Cube position and rotation speed variables.
+int WINDOW_ID;
+int WINDOW_WIDTH = 600;
+int WINDOW_HEIGHT = 400;
+
 float rotation_degrees = 0.0;
-float X_Rot = 0.0; 
-float Y_Rot = 0.0;
-float X_Speed = 0.0f;  // the rotation.
-float Y_Speed = 0.0f;
-float Z_Off   =-10.0f;
 
-float cx = 0.0; 
-float cy = 0.0; 
-float cz = 0.0;
+// Set in main from user parameters
+float cube_center_x = 0.0; 
+float cube_center_y = 0.0; 
+float cube_center_z = 0.0;
 
+// Set in main from user parameters
 float rotation_vector_x = 0.0;
 float rotation_vector_y = 0.0;
 float rotation_vector_z = 0.0;
 
-//////////////////////////////////////////////////////////
-// String rendering routine; leverages on GLUT routine. //
-//////////////////////////////////////////////////////////
 static void PrintString(void *font, char *str)
 {
    int i,len=strlen(str);
@@ -56,20 +48,27 @@ void CallBackRenderScene(void)
    glLoadIdentity(); 
 
    // Move the object back from the screen.
-   glTranslatef(0.0f,0.0f, Z_Off);
+   glTranslatef(0.0f, 0.0f, -10.0);
 
-   // Rotate the calculated amount.
-   glTranslatef(-cx, -cy, -cz);
-   glRotatef(rotation_degrees, rotation_vector_x, rotation_vector_y, rotation_vector_z);
-   glRotatef(rotation_degrees, rotation_vector_x, rotation_vector_y, rotation_vector_z);
-   glTranslated(cx, cy, cz);
+   // Translate the cube to the origin
+   glTranslatef(-cube_center_x, -cube_center_y, -cube_center_z);
+
+   // Rotate the cube 15 degrees
+   glRotatef(
+      rotation_degrees, 
+      rotation_vector_x,
+      rotation_vector_y, 
+      rotation_vector_z
+   );
+
+   // Translate the cube back to its original position
+   glTranslatef(cube_center_x, cube_center_y, cube_center_z);
 
    // Clear the color and depth buffers.
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-   // OK, let's start drawing our planer quads.
    glBegin(GL_QUADS); 
-      
+
       // Far face. 
       glVertex3f(-1.0f, -1.0f, -1.0f);
       glVertex3f(-1.0f,  1.0f, -1.0f);
@@ -126,7 +125,7 @@ void CallBackRenderScene(void)
    // Display a string
    // Now we set up a new projection for the text.
    glLoadIdentity();
-   glOrtho(0,Window_Width,0,Window_Height,-1.0,1.0);
+   glOrtho(0,WINDOW_WIDTH,0,WINDOW_HEIGHT,-1.0,1.0);
    glColor4f(0.6,1.0,0.6,1.00);
    sprintf(buf,"%s", DISPLAY_INFO); // Print the string into a buffer
    glRasterPos2i(2,2);                         // Set the coordinate
@@ -135,7 +134,7 @@ void CallBackRenderScene(void)
    
    // To ease, simply translate up.  Note we're working in screen
    // pixels in this projection.
-   glTranslatef(6.0f, Window_Height - 14, 0.0f);
+   glTranslatef(6.0f, WINDOW_HEIGHT - 14, 0.0f);
 
    // Done with this special projection matrix.  Throw it away.
    glPopMatrix();
@@ -146,40 +145,6 @@ void CallBackRenderScene(void)
 
    // All done drawing.  Let's show it.
    glutSwapBuffers();
-
-   // Now let's do the motion calculations.
-   X_Rot+=X_Speed; 
-   Y_Rot+=Y_Speed; 
-}
-
-/////////////////////////////////////////////////////////////
-// Callback Function called when a special key is pressed. //
-/////////////////////////////////////////////////////////////
-void CallBackSpecialKeyPressed(int key, int x, int y)
-{
-   switch (key) {
-   case GLUT_KEY_PAGE_UP: // move the cube into the distance.
-      Z_Off -= 0.05f;
-      break;
-   case GLUT_KEY_PAGE_DOWN: // move the cube closer.
-      Z_Off += 0.05f;
-      break;
-   case GLUT_KEY_UP: // decrease x rotation speed;
-      X_Speed -= 0.01f;
-      break;
-   case GLUT_KEY_DOWN: // increase x rotation speed;
-      X_Speed += 0.01f;
-      break;
-   case GLUT_KEY_LEFT: // decrease y rotation speed;
-      Y_Speed -= 0.01f;
-      break;
-   case GLUT_KEY_RIGHT: // increase y rotation speed;
-      Y_Speed += 0.01f;
-      break;
-   default:
-      printf ("SKP: No action for %d.\n", key);
-      break;
-    }
 }
 
 ///////////////////////////////////////////////////////////////
@@ -199,8 +164,8 @@ void CallBackResizeScene(int Width, int Height)
 
    glMatrixMode(GL_MODELVIEW);
 
-   Window_Width  = Width;
-   Window_Height = Height;
+   WINDOW_WIDTH  = Width;
+   WINDOW_HEIGHT = Height;
 }
 
 ////////////////////////////////////////////////////////
@@ -224,15 +189,14 @@ void MyInit(int Width, int Height)
    CallBackResizeScene(Width,Height);
 }
 
-void myCBKey(unsigned char key, int x, int y) {
-   /* 
-   If 'a' if pressed, move the sphere away from the camera. 
-   If 'z' is pressed, move the sphere toward the camera. 
-   */
+void quit_key_function(unsigned char key, int x, int y) {
    switch(key) {
+      case 81: // Q
+         // Destroy the window and quit.
+         glutDestroyWindow(WINDOW_ID);
       case 113: // q
 	      // Destroy the window and quit.
-	      glutDestroyWindow(Window_ID);
+	      glutDestroyWindow(WINDOW_ID);
 	      exit(1);
          break; 
       default: 
@@ -256,44 +220,49 @@ void mouse_function(int button, int state, int x, int y) {
    }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
    glutInit(&argc, argv);   
    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
-   glutInitWindowSize(Window_Width, Window_Height);
-   Window_ID = glutCreateWindow(PROGRAM_TITLE);
+   glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+   WINDOW_ID = glutCreateWindow(PROGRAM_TITLE);
 
-   // Register and install the callback function to do the drawing. 
+   // Register callback functions
    glutDisplayFunc(&CallBackRenderScene);
-   // If there's nothing to do, draw.
    glutIdleFunc(&CallBackRenderScene);
-
    glutReshapeFunc(&CallBackResizeScene);
-
-   // Register and install the callback function for keys
-   glutKeyboardFunc(&myCBKey);
-   glutSpecialFunc(&CallBackSpecialKeyPressed);
+   glutKeyboardFunc(&quit_key_function);
    glutMouseFunc(&mouse_function);
 
-   MyInit(Window_Width, Window_Height);
+   MyInit(WINDOW_WIDTH, WINDOW_HEIGHT);
 
+   // Now take in command line arguments
+   if (argc != 10) {
+      printf("Not enough parameters passed\n");
+      return 0; 
+   }
+   
+   // Point 1 of vector used as axis
    float p1x = atof(argv[1]);
    float p1y = atof(argv[2]);
    float p1z = atof(argv[3]);
 
+   // Point 2 of vector used as axis
    float p2x = atof(argv[4]);
    float p2y = atof(argv[5]);
    float p2z = atof(argv[6]);
 
+   // Find the vector by subtracting 
+   // point 1 from point 2
    rotation_vector_x = (p2x - p1x);
    rotation_vector_y = (p2y - p1y);
    rotation_vector_z = (p2z - p1z);
-   
-   cx = atof(argv[7]); 
-   cy = atof(argv[8]);
-   cz = atof(argv[9]);
 
+   // Define the cube center point 
+   // of which we rotate around. 
+   cube_center_x = atof(argv[7]); 
+   cube_center_y = atof(argv[8]);
+   cube_center_z = atof(argv[9]);
 
    // Print out a bit of help dialog.
    printf("\n%s\n\n", PROGRAM_TITLE);
