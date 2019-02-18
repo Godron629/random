e821fc9f2e1f8bdb7b967cf70835570b61b197d6
gideon <gideonrich@hotmail.ca>
2019-02-18 16:42:17 -0700

    Add more to recursion_3

--- racket/recursion_3.rkt
+++ racket/recursion_3.rkt
@@ -33,3 +33,53 @@
   (cond
     ((null? ls) '())
     (else (cons (list (car ls)) (deepen-1 (cdr ls))))))
+
+(define (=append= ls1 ls2)
+  ;; append two lists
+  (cond
+    ((null? ls1) ls2)
+    (else (cons (car ls1) (=append= (cdr ls1) ls2)))))
+
+(define (subst-all new old ls)
+  ;; substitute all top level and nested
+  ;; occurences of `old` with `new` in a list.
+  (cond
+    ((null? ls) '())
+    ((equal? old (car ls))
+     (cons new (subst-all new old (cdr ls))))
+    ((pair? (car ls))
+     (cons (subst-all new old (car ls)) (subst-all new old (cdr ls))))
+    (else
+     (cons (car ls) (subst-all new old (cdr ls))))))
+
+(define (insert-left-all new old ls)
+  ;; Put a `new` to the left of every `old`
+  ;; at the top and nested levels of list.
+  (cond
+    ((null? ls) '())
+    ((equal? old (car ls))
+     (cons new (cons old (insert-left-all new old (cdr ls)))))
+    ((pair? (car ls))
+     (cons (insert-left-all new old (car ls))
+           (insert-left-all new old (cdr ls))))
+    (else
+     (cons (car ls) (insert-left-all new old (cdr ls))))))
+
+(define (sum-all ls)
+  ;; sum top and nested numbers in a list
+  (cond
+    ((null? ls) 0)
+    ((pair? (car ls))
+     (+ (sum-all (car ls))
+        (sum-all (cdr ls))))
+    (else (+ (car ls) (sum-all (cdr ls))))))
+
+
+
+
+
+
+
+
+
+
