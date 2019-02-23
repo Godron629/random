5a39fe40fd7714617e140793afce4932dd9ceed6
Gideon <gideonrich@hotmail.ca>
2019-02-23 10:22:31 -0700

    format with black

--- derangement.py
+++ derangement.py
@@ -1,4 +1,3 @@
-
 import unittest
 from math import floor, e
 from io import StringIO
@@ -7,45 +6,46 @@ from io import StringIO
 # Derangement code golf: see `derangement` definition.
 # `(n-1)*-1` Is the base case, converting 0 to 1 and 1 to 0.
 #    This was used as elif cannot be used and double lambda is lame.
-d = lambda n: (n-1)*-1 if n in [0,1] else n*(d(n-1)) + (-1)**n
+d = lambda n: (n - 1) * -1 if n in [0, 1] else n * (d(n - 1)) + (-1) ** n
 
 
 def derangement(n: int) -> int:
-    '''
+    """
     Calculate derangement of a number.
 
     I.e. the number of permutations of n where no element appears
     in its original position.
 
     https://en.wikipedia.org/wiki/Derangement
-    '''
+    """
     if n < 0:
-        raise ValueError('Derangement only defined for n > -1')
+        raise ValueError("Derangement only defined for n > -1")
     if n == 0:
         return 1
     if n == 1:
         return 0
     return nearest_integer(factorial(n) / e)
 
+
 def nearest_integer(n: float) -> int:
-    '''
+    """
     Return the nearest integer of n
-    '''
-    return int(floor(n + 0.5 ))
+    """
+    return int(floor(n + 0.5))
+
 
 def factorial(n: int) -> int:
-    '''
+    """
     Return factorial (n!) of n
-    '''
+    """
     if n < 0:
-        raise ValueError('Factorial only defined for n > -1')
+        raise ValueError("Factorial only defined for n > -1")
     if n in [0, 1]:
         return 1
     return n * factorial(n - 1)
 
 
 class TestDerangement(unittest.TestCase):
-
     def test_normal_integer(self):
         self.assertEqual(265, derangement(6))
 
@@ -60,7 +60,7 @@ class TestDerangement(unittest.TestCase):
             derangement(-1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     print("Running code golf...\n")
     for n in [0, 1, 6, 9]:
@@ -71,10 +71,10 @@ if __name__ == '__main__':
     suite = unittest.TestSuite()
 
     test_cases = [
-        'test_normal_integer',
-        'test_base_case_one_returns_zero',
-        'test_base_case_zero_returns_one',
-        'test_negative_raises_valueerror',
+        "test_normal_integer",
+        "test_base_case_one_returns_zero",
+        "test_base_case_zero_returns_one",
+        "test_negative_raises_valueerror",
     ]
 
     for t in test_cases:
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     # "You'll be given inputs as one integer per line."
     #  "Your program should yield the subfactorial result."
     #  Pff, files? What is this - 1995?
-    challenge_input = '6\n9\n14\n'
+    challenge_input = "6\n9\n14\n"
     file = StringIO(challenge_input)
     for line in file:
         print(derangement(int(line)))
